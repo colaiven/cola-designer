8c7842d06749e2463b79f708c0590cb6033a6df7
chao <vchenghou@aliyun.com>
2021-04-24 18:56:48 +0800

    引入echarts

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import Preview from '@/preview/index'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import echarts from 'echarts'
 
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 

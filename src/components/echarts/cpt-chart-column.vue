438f46ed37de858701d026dcf9cbddaf6e848cc5
chao <vchenghou@aliyun.com>
2021-04-25 18:58:48 +0800

    引入UUID

--- src/components/echarts/cpt-chart-column.vue
+++ src/components/echarts/cpt-chart-column.vue
@@ -1,10 +1,25 @@
 <template>
-  <div></div>
+  <div :id="uuid"></div>
 </template>
 
 <script>
 export default {
-  name: "cpt-chart-column"
+  name: "cpt-chart-column",
+  title: "按钮",
+  icon: 'el-icon-thumb',
+  initWidth: 400,
+  initHeight: 300,
+  group: 'echarts',
+  props:{option:Object},
+  data(){
+    return {
+      uuid:''
+    }
+  },
+  created() {
+    this.uuid = require('uuid');
+    console.log()
+  }
 }
 </script>
 

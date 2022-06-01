86a243660b6771937164578ef3811685b4d2a285
chao <vchenghou@aliyun.com>
2022-06-01 20:09:46 +0800

    sync1.8.9

---
+++ src/utils/myutils.js
@@ -0,0 +1,12 @@
+export function isJSON(str) {
+    if (typeof str == 'string') {
+        try {
+            JSON.parse(str)
+            return true
+        } catch (e) {
+            return false
+        }
+    } else {
+        return false
+    }
+}
